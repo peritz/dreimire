46e721a07d063c2e6a3cfe1a796d679c8d0f4ff3
Peritz the Penguin <peritz@pardonmynoot.com>
2026-09-06 12:27:13 +0100

    Add login form with bootstrap and HTMX templates in base

--- src/dreimire/routes.py
+++ src/dreimire/routes.py
@@ -1,7 +1,25 @@
+from flask import render_template, flash, redirect, url_for
 from dreimire import app
+from dreimire.forms import LoginForm
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "Ryan"}
+    return render_template("index.html", title="Home", user=user)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/profile")
+def profile():
+    return "Undefined"
