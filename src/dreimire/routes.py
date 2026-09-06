2e08719fdda6b47966a75b554679de19b3a69c24
Peritz the Penguin <peritz@pardonmynoot.com>
2026-09-06 16:21:06 +0100

    Add user registration form

--- src/dreimire/routes.py
+++ src/dreimire/routes.py
@@ -5,7 +5,7 @@ from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from dreimire import app, db
-from dreimire.forms import LoginForm
+from dreimire.forms import LoginForm, RegistrationForm
 from dreimire.models import User
 
 
@@ -19,8 +19,12 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    next_page = request.args.get("next")
+    if not next_page or urlsplit(next_page).netloc != "":
+        next_page = url_for("index")
+
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(next_page)
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(
@@ -30,11 +34,29 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get("next")
-        if not next_page or urlsplit(next_page).netloc != "":
-            next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    next_page = request.args.get("next")
+    if not next_page or urlsplit(next_page).netloc != "":
+        next_page = url_for("index")
+
+    if current_user.is_authenticated:
+        return redirect(next_page)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(
+            "Congratulations, you are now a registered user! Please login to continue."
+        )
+        return redirect(url_for("login", next=next_page))
+    return render_template("register.html", title="Register", form=form)
 
 
 @app.route("/logout")
